c67422b0463f3527f0e3d4cf10caf4a59f9d622d
Aman5681 <gandhi.aman.5681@gmail.com>
2022-07-16 18:35:42 +0530

    initial commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+module.exports = {
+  images: {
+    domains: ["links.papareact.com", "image.tmdb.org"],
+  },
+};
